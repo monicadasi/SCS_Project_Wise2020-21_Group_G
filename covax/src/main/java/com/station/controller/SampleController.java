1415cd09791680b1abd505fc4f538fef2ce05852
nikhiljohnbejoy <nikhiljohnbejoy@yahoo.co.in>
2020-12-28 18:17:38 +0100

    emailing trial

--- covax/src/main/java/com/station/controller/SampleController.java
+++ covax/src/main/java/com/station/controller/SampleController.java
@@ -1,8 +1,14 @@
 package com.station.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.station.services.EmailSender;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -12,5 +18,13 @@ public class SampleController {
 	public String isWorking() {
 		System.out.println("got req");
 		return "{\"value\":\"WORKING\"}";
+	}
+	@RequestMapping(
+		    value = "/sendtoken", 
+		    method = RequestMethod.POST)
+	public String sendMail(@RequestBody Map<String, Object> payload) {
+		System.out.println(payload);
+		int token=EmailSender.sendEmail((String) payload.get("email"));
+		return "{\"token\":\""+token+"\"}";
 	}
 }
